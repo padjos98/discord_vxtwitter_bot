8e664d3d1d60801df31266dfca697324835dc582
padjos98 <jppadg98@gmail.com>
2025-02-16 19:04:24 -0500

    5

--- vxtwitterlink.py
+++ vxtwitterlink.py
@@ -1,12 +1,11 @@
 def vxtwitterlinkfunc(xtwitterlink):
-	
     #define xtwitter and vxtwitter urls
     xtwitterurl = "https://x.com/"
     vxtwitterurl = "https://vxtwitter.com/"
 
     #If link is good replace with vxtwitter link and return string
     if  xtwitterurl in xtwitterlink:
-		vxtwitterlink = xtwitterlink.replace(xtwitterurl, vxtwitterurl)
+        vxtwitterlink = xtwitterlink.replace(xtwitterurl, vxtwitterurl)
         return vxtwitterlink
     #check to see if it is a twitter link if not return -1
     else:
