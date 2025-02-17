c1a9e1b3b58060fe9c4ac8f90e8f765c745a04f6
padjos98 <jppadg98@gmail.com>
2025-02-16 21:17:32 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,10 +8,13 @@ import vxtwitterlink
 #Access the .env file
 load_dotenv()
 TOKEN = os.environ.get('BOT_TOKEN')
-GENERAL_CHANNEL_ID=470047029251407875
 
 #Define the bot for all events and command handling
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+#Declare general channel
+GENERAL_CHANNEL_ID=470047029251407875
+general_channel = bot.get_channel(GENERAL_CHANNEL_ID)
 
 #Confrim bot is ready in terminal
 @bot.event
@@ -22,16 +25,16 @@ async def on_ready():
 #Message handling when a user sends a messgae
 @bot.event
 async def on_message(message):
+    #Run script to swap x url 
     user_message = message.content
     link = vxtwitterlink.vxtwitterlinkfunc(user_message)
     
+	#Determine if the bot needs to delete user's link and replace with their own updated link
     if link == -1:
         return
     else:
         await message.delete()
-        channel = bot.get_channel(GENERAL_CHANNEL_ID)
-        await channel.send(link)
-        
-			
+        await general_channel.send(link)
+        		
 #Run the bot
 bot.run(TOKEN)
