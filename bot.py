4ab1043ee234149a1d649c1820e5399b3233db7c
padjos98 <jppadg98@gmail.com>
2025-02-16 19:48:27 -0500

    1

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ async def on_message(message):
 		user_message = message.content
 		link = vxtwitterlink.vxtwitterlinkfunc(user_message)
 
-		if user_message == None:
+		if user_message == "-1":
 			print("Invalid Link")
 		else:
 			print(link)
