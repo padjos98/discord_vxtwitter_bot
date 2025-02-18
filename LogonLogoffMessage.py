260a2d6972ce580671b345a3f61f8814b11818e0
padjos98 <jppadg98@gmail.com>
2025-02-17 20:09:04 -0500

    Update LogonLogoffMessage.py

--- LogonLogoffMessage.py
+++ LogonLogoffMessage.py
@@ -12,22 +12,24 @@ def MessageCreate(member, before, after):
     current_channel = after.channel
     previous_channel = before.channel
     message_color = 'red'
+    voice_state = ""
 
     #Choose color base on what the user did
     print(before)
     print(after)
-    if previous_channel == None:
-        message_colorcolor = 'green'
-    elif current_channel == None:
-        message_colorcolor = 'red'
-    else:
-        message_colorcolor = 'yellow'
-
+        
     if current_channel == None:
         current_channel = before.channel
+        message_colorcolor = 'red'
+        voice_state = "Voice Leave"
     elif previous_channel == None:
         current_channel = after.channel
-
+        message_colorcolor = 'green'
+        voice_state = "Voice Join"
+    else:
+        current_channel = after.channel
+        message_colorcolor = 'yellow'
+        voice_state = "Voice Switch"
 
     #Create the embedded message
     embed = discord.Embed(color=message_color)
@@ -35,7 +37,7 @@ def MessageCreate(member, before, after):
     #Add fields to the embed
     #embed.set_thumbnail(url=profile_pic)
     embed.add_field(name=current_user, value="", inline=True)
-    embed.add_field(name="Voice Leave", value="", inline=False)
+    embed.add_field(name=voice_state, value="", inline=False)
     embed.add_field(name="User", value=member.mention, inline=True)
     embed.add_field(name="Channel", value=current_channel.mention, inline=True)
     embed.add_field(name=now.strftime("%m/%d/%y %H:%-I:%S %p"), value="", inline=False)  
